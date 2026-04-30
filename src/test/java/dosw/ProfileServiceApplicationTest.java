74876271f2749692eda1099aca6a5d6595df67a9
Andres Sabogal <andres.sabogal-w@mail.escuelaing.edu.co>
2026-04-30 00:02:01 -0500

    pipeline terminada

---
+++ src/test/java/dosw/ProfileServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package dosw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileServiceApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
